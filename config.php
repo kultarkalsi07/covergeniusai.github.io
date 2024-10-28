3a736a32af3997426a0e0deed7d7637ec1a1f8c8
kultarkalsi07 <kalsikultar@gmail.com>
2024-10-28 06:10:08 +0530

    update 3

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 // config.php
 // Save this in a location NOT accessible from the web
-define('GEMINI_API_KEY', 'YOUR_API_KEY_HERE');
+define('GEMINI_API_KEY', 'AIzaSyDhqMqdvA6HUy3AQ-1_9Rtc20jFIKSy2qw');
